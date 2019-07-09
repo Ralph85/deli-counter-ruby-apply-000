aed168fa60ec6cd23f18ab8793cd0e9efa5358c4
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-09 17:45:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,37 +1,29 @@
-
+katz_deli = []
 
 def line(katz_deli)
+  line_positions = [];
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each.with_index(1) do |name, index|
-      puts "The line is currently: #{index}. << #{name}"
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
     end
-  end
-end
-
-def line(x)
-  line_array = []
-  if x.length == 0
-    puts "The line is currently empty."
-  else
-    x.each.with_index(1) do |name, index|
-      line_array.push("#{index}. #{index}")
-    end
-    puts "The line is currently: #{line_array.join(" ")}"
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are Number #{katz_deli.length} in line."
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving(array)
-  if array.empty?
+def now_serving(katz_deli)
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{arryay[0]}."
-    array.shift
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   end
 end
