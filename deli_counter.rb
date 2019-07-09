a3b833c644b3d5fd8da663fe66df3c907fe720a9
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-09 17:33:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,30 @@ def line(katz_deli)
     end
   end
 end
+
+def line(x)
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{index}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are Number #{katz_deli.length} in line."
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{arryay[0]}."
+    array.shift
+  end
+end
+
